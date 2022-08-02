167cb70465a7ff389e04f3474b91c57f89e59601
RocioAlday <roo.alday@gmail.com>
2022-08-02 19:08:54 -0300

    Homework M1-01

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,15 +1,25 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+  
+ let resultado= 0;
+ let numReverse= num.split('').reverse().join('');
+ for(let i=0; i<numReverse.length; i++){
+  resultado+= numReverse[i]*2**i;
+ }
+ return resultado;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  
+let binario= '';
+while (num!==0){
+  binario+= num%2;
+  num= Math.floor(num/2);
 }
-
+binario= binario.split('').reverse().join('')
+return binario;
+}
 
 module.exports = {
   BinarioADecimal,
