c23eac25cd8389f9b779c235f30556d41892dd69
RocioAlday <roo.alday@gmail.com>
2022-08-04 20:02:37 -0300

    Otras formas de resolverlo

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -3,22 +3,21 @@
 function BinarioADecimal(num) {
   
  let resultado= 0;
- let numReverse= num.split('').reverse().join('');
+ let numReverse= num.split('').reverse().join('');  // puedo sacar el join y en el resultado agrego:
  for(let i=0; i<numReverse.length; i++){
-  resultado+= numReverse[i]*2**i;
+  resultado+= numReverse[i]*2**i;       // Math.pow(2, i)* parseInt (numReverse[i])
  }
  return resultado;
 }
 
-function DecimalABinario(num) {
-  
-let binario= '';
-while (num!==0){
-  binario+= num%2;
-  num= Math.floor(num/2);
+function DecimalABinario(num) {    // puedo resolverlo con un array
+                                    // let result= [];
+let binario= '';                    // no uso binario
+while (num!==0){                    // while (num>0)
+  binario+= num%2;                  // result.push (num%2);
+  num= Math.floor(num/2);           // igual
 }
-binario= binario.split('').reverse().join('')
-return binario;
+return binario.split('').reverse().join('');  //no me haria falta el split xq ya es un arreglo el result
 }
 
 module.exports = {
