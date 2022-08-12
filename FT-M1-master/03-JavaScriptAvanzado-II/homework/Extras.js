ca166b5ce2aab86c4c6f7e00b4da332360a5e9e6
RocioAlday <roo.alday@gmail.com>
2022-08-12 20:25:08 -0300

    Extras 03

--- FT-M1-master/03-JavaScriptAvanzado-II/homework/Extras.js
+++ FT-M1-master/03-JavaScriptAvanzado-II/homework/Extras.js
@@ -6,9 +6,83 @@ String.prototype.repeatify = function(numero){
     }
     var result='';
     while(numero>0){                //se puede plantear con un for tmb
-        result+= this;              
+        result+= this;              // ver otras forma que no sea guardarndo this en una variable
         numero-=1;
     }
     return result;
 }
-console.log("Buenas!".repeatify(3));
+//console.log("Buenas!".repeatify(3));
+
+
+class Shape {
+    constructor(){
+    this.type= 'Shape';
+    this.getType= function(){
+        return this.type;
+    }
+    }
+}
+/*/ OTRA FORMA DE CREAR SHAPE ES DIRECTAMENTE COMO UN OBJETO:
+var Shape= {
+    type: 'Shape',  
+    getType: function(){
+        return this.type;
+    }
+};*/
+
+class Triangle extends Shape {
+    constructor(a, b, c){
+        super();
+        this.type= 'Triangle';
+        this.a=a;
+        this.b=b;
+        this.c=c;
+    }
+}
+/*/   OTRA FORMA PARA CREAR TRIANGLE ES DIRECTAMENTE EXPRESARLO COMO UNA FUNCION
+function Triangle (a, b, c) {   
+this.type='Triangle';
+this.a=a;
+this.b=b;
+this.c=c;
+} */
+//Triangle.prototype= Shape;
+//Triangle.prototype.constructor= Triangle;
+
+Triangle.prototype.getPerimeter= function(){
+    return this.a + this.b + this.c;
+}
+
+var t = new Triangle(1, 2, 3);
+console.log (t instanceof Triangle);
+// true
+console.log(Shape.prototype.isPrototypeOf(t));
+// true
+ console.log (t.getPerimeter());
+// 6
+console.log (t.getType());
+// "Triangle"
+
+
+class Circle extends Shape {
+    constructor(r){
+        super();
+        this.type= "Circle";
+        this.radio=r;
+}
+getPerimeter(){
+    let pi= 3.14;
+    return 2*pi*this.radio;
+}
+}
+
+var c = new Circle(2);
+console.log (c instanceof Circle);
+// true
+console.log(Shape.prototype.isPrototypeOf(c));
+// true
+console.log (c.getPerimeter());
+// 12.566370614359172
+console.log (c.getType());
+// "Circle"
+
